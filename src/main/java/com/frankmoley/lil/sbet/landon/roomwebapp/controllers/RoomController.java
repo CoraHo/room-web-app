618d171a8a7336fd92c48bd2df623f5ba65dcbae
CoraHo <cora9122@gmail.com>
2022-07-07 23:55:46 +0800

    Initial commit

---
+++ src/main/java/com/frankmoley/lil/sbet/landon/roomwebapp/controllers/RoomController.java
@@ -0,0 +1,33 @@
+package com.frankmoley.lil.sbet.landon.roomwebapp.controllers;
+
+
+import com.frankmoley.lil.sbet.landon.roomwebapp.models.Room;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class RoomController {
+
+    private static final List<Room> rooms = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10; i++) {
+            rooms.add(new Room(i, "Room" + i, "R" + i, "Q"));
+        }
+    }
+
+    @GetMapping("/rooms")
+    public String getAllRooms() {
+        return "rooms";
+    }
+
+//    public String getAllRooms(Model model) {
+//        model.addAttribute("rooms", rooms);
+//        return "rooms";
+//    }
+}
